7827091b4eefe7df1036b8ceaeca45aae7f32e5d
Yves Desjardins <YvesDesjardins@gmail.com>
2019-02-06 11:20:04 -0800

    Initial code

---
+++ count.js
@@ -0,0 +1,14 @@
+// returns an object all unique characters in a string and how many instances of each character there are
+function countLetters(str) {
+    var out = {};
+
+    for (var char in str) {
+        if (!out[char]) {
+            out[char] = 1;
+        } else {
+            out[char]++;
+        }
+    }
+
+    return out;
+}
